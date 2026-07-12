6f886a25798572ddbac61b5c1283911ea873b0d2
MdHasibAskari <mdhsbaskari@gmail.com>
2026-07-12 14:41:18 +0600

    refact: renamed services for better clarity

--- Homework01/Program.cs
+++ Homework01/Program.cs
@@ -5,57 +5,60 @@ using Homework01.Services;
 var services = new ServiceCollection();
 
 services.AddTransient<IMyService, MyService>();
-services.AddSingleton<IMyService, MyService>(); // duplicate registration for demonstration purposes
+
+// services.AddSingleton<IMyService, MyService>(); // duplicate registration for demonstration purposes
 services.AddSingleton<IEmailService, EmailService>();
 services.AddScoped<IScopedService, ScopedService>();
 
 var provider = services.BuildServiceProvider();
 
-var a = provider.GetService<IMyService>();
-var b = provider.GetService<IMyService>();
+var transientSvc1 = provider.GetService<IMyService>();
+var transientSvc2 = provider.GetService<IMyService>();
 
-var s1 = provider.GetService<IMyService>();
-var s2 = provider.GetService<IMyService>();
+// var s1 = provider.GetService<IMyService>();
+// var s2 = provider.GetService<IMyService>();
 
-var e1 = provider.GetService<IEmailService>();
-var e2 = provider.GetService<IEmailService>();
+var singletonSvc1 = provider.GetService<IEmailService>();
+var singletonSvc2 = provider.GetService<IEmailService>();
 
 using (var scope = provider.CreateScope())
 {
-    var scopedService1 = scope.GetService<IScopedService>();
-    var scopedService2 = scope.GetService<IScopedService>();
+    var scopedSvc1 = scope.GetService<IScopedService>();
+    var scopedSvc2 = scope.GetService<IScopedService>();
 
     Console.WriteLine("Scoped Example:");
-    Console.WriteLine(scopedService1.GetGuid());
-    Console.WriteLine(scopedService2.GetGuid());
+    Console.WriteLine(scopedSvc1.GetGuid());
+    Console.WriteLine(scopedSvc2.GetGuid());
 }
 
 Console.WriteLine();
 
 using (var scope = provider.CreateScope())
 {
-    var scopedService1 = scope.GetService<IScopedService>();
-    var scopedService2 = scope.GetService<IScopedService>();
+    var scopedSvc1 = scope.GetService<IScopedService>();
+    var scopedSvc2 = scope.GetService<IScopedService>();
 
     Console.WriteLine("Scoped Example:");
-    Console.WriteLine(scopedService1.GetGuid());
-    Console.WriteLine(scopedService2.GetGuid());
+    Console.WriteLine(scopedSvc1.GetGuid());
+    Console.WriteLine(scopedSvc2.GetGuid());
 }
 
 Console.WriteLine();
 
 Console.WriteLine("Transient Example:");
-Console.WriteLine(a.GetGuid());
-Console.WriteLine(b.GetGuid());
+Console.WriteLine(transientSvc1.GetGuid());
+Console.WriteLine(transientSvc2.GetGuid());
 
 Console.WriteLine();
 
 Console.WriteLine("Singleton Example:");
-Console.WriteLine(e1.GetGuid());
-Console.WriteLine(e2.GetGuid());
+Console.WriteLine(singletonSvc1.GetGuid());
+Console.WriteLine(singletonSvc2.GetGuid());
 
-Console.WriteLine("Singleton Example (duplicate):");
-Console.WriteLine(s1.GetGuid());
-Console.WriteLine(s2.GetGuid());
-
-Console.WriteLine();
+// Console.WriteLine();
+//
+// Console.WriteLine("Singleton Example (duplicate):");
+// Console.WriteLine(s1.GetGuid());
+// Console.WriteLine(s2.GetGuid());
+//
+// Console.WriteLine();
